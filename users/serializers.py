4022275dbed7108d1d022e8cd7d20d99c009c05a
Miguel <miguel@sopinet.com>
2018-12-16 16:38:34 +0100

    Fixed encrypt password

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
@@ -9,6 +10,21 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email', 'username', 'password']
+
+    def encrypt_password(self, validated_data):
+        password = validated_data.get('password')
+        if password:
+            validated_data['password'] = make_password(password)
+
+    def create(self, validated_data):
+        self.encrypt_password(validated_data)
+        user = super().create(validated_data)
+        return user
+
+    def update(self, instance, validated_data):
+        self.encrypt_password(validated_data)
+        user = super().update(instance, validated_data)
+        return user
 
 
 class UserListSerializer(serializers.HyperlinkedModelSerializer):
