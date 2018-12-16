2d78c7f4fe2a37ef42e32175578385cedf73f46a
Miguel <miguel@sopinet.com>
2018-12-16 16:27:32 +0100

    Fix create users without firstname or lastname

--- users/serializers.py
+++ users/serializers.py
@@ -10,19 +10,6 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['first_name', 'last_name', 'email', 'username', 'password']
 
-    def create(self, validated_data):
-        user = User()
-        return self.update(user, validated_data)
-
-    def update(self, instance, validated_data):
-        instance.first_name = validated_data.get('first_name')
-        instance.last_name = validated_data.get('last_name')
-        instance.username = validated_data.get('username')
-        instance.email = validated_data.get('email')
-        instance.set_password(validated_data.get('password'))
-        instance.save()
-        return instance
-
 
 class UserListSerializer(serializers.HyperlinkedModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name='blog', read_only=True)
